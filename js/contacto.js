eb9b25ae744242bafb35c9dd08d633db755046d2
Juan Principi <principijuan@gmail.com>
2023-05-02 16:51:09 -0300

    final commit

--- js/contacto.js
+++ js/contacto.js
@@ -1,19 +1,30 @@
-const form = document.getElementById("contact-form");
+const btn = document.getElementById("formButton");
 
-form.addEventListener("submit", (event) => {
+document.getElementById("form").addEventListener("submit", function (event) {
   event.preventDefault();
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const message = document.getElementById("message").value;
-  const data = {
-    name,
-    email,
-    message,
-  };
-  const xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://formspree.io/f/xvovvrql", true);
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.send(JSON.stringify(data));
-  alert("¡Gracias por contactarnos! Nos pondremos en contacto contigo pronto.");
-  form.reset();
+
+  btn.value = "Enviando...";
+
+  const serviceID = "default_service";
+  const templateID = "template_1igf6ty";
+
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      btn.value = "Enviar consulta";
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "¡Consulta enviada!",
+        text: "Se enviará un correo de confirmación al email que ingresaste",
+        showConfirmButton: true,
+        timerProgressBar: true,
+        timer: 4000,
+      });
+      this.reset();
+    },
+    (err) => {
+      btn.value = "Enviar consulta";
+      alert(JSON.stringify(err));
+    }
+  );
 });
